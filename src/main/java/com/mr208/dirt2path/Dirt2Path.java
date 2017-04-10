0625e570df173fe56cd1a34d3739ef85081ae187
Andrew W <marwat208@gmail.com>
2017-04-09 20:35:29 -0400

    Now with Take Backsies Right Click on a Path Block to convert it back to Dirt

--- src/main/java/com/mr208/dirt2path/Dirt2Path.java
+++ src/main/java/com/mr208/dirt2path/Dirt2Path.java
@@ -10,6 +10,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -20,7 +21,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = Dirt2Path.MOD_ID, name = Dirt2Path.MOD_NAME, version = "1.2.0", acceptedMinecraftVersions = "[1.9,1.12)")
+@Mod(modid = Dirt2Path.MOD_ID, name = Dirt2Path.MOD_NAME, version = "1.3.0", acceptedMinecraftVersions = "[1.9,1.12)")
 public class Dirt2Path {
 
 	public static final String MOD_ID = "dirt2path";
@@ -41,29 +42,44 @@ public class Dirt2Path {
 	@SubscribeEvent
 	public void onBlockRightclick(PlayerInteractEvent.RightClickBlock event) {
 
-		EntityPlayer player = event.getEntityPlayer();
-		World world = event.getWorld();
-		BlockPos blockPos = event.getPos();
-		ItemStack itemStack = player.getHeldItemMainhand();
+			EntityPlayer player = event.getEntityPlayer();
+			World world = event.getWorld();
+			BlockPos blockPos = event.getPos();
+			ItemStack itemStack = player.getHeldItem(event.getHand());
 
-		if(itemStack==null || itemStack==EMPTY) return;
+			if (itemStack == null || itemStack == EMPTY)
+				return;
 
-		if(!itemStack.canHarvestBlock(Blocks.SNOW.getDefaultState())) return;
+			if (!itemStack.canHarvestBlock(Blocks.SNOW.getDefaultState()))
+				return;
 
-		IBlockState iBlockState = world.getBlockState(blockPos);
+			IBlockState iBlockState = world.getBlockState(blockPos);
 
-		if(world.getBlockState(blockPos.up()).getMaterial() == Material.AIR && isBlockDirt(iBlockState)) {
-			IBlockState pathState = getPathBlockState(iBlockState);
-			world.playSound(player, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
-
-			player.swingArm(EnumHand.MAIN_HAND);
-
-			if(!world.isRemote) {
-
-				world.setBlockState(blockPos, pathState, 11);
-				itemStack.damageItem(1, player);
+			if (world.getBlockState(blockPos.up()).getMaterial() == Material.AIR) {
+				if (isBlockDirt(iBlockState)) {
+					IBlockState pathState = getPathBlockState(iBlockState);
+					setPathOrDirt(world, pathState, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, player, itemStack);
+				} else if (isBlockPath(iBlockState)) {
+					IBlockState dirtState = getDirtBlockState(iBlockState);
+					setPathOrDirt(world, dirtState, blockPos, SoundEvents.ITEM_HOE_TILL, player, itemStack);
+				}
 			}
+	}
+
+	protected void setPathOrDirt(World world, IBlockState blockState, BlockPos blockPos, SoundEvent soundEvent, EntityPlayer player, ItemStack itemStack) {
+		world.playSound(player, blockPos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F);
+		player.swingArm(EnumHand.MAIN_HAND);
+		if(!world.isRemote) {
+			world.setBlockState(blockPos, blockState, 11);
+			itemStack.damageItem(1, player);
 		}
+	}
+
+	protected boolean isBlockPath(IBlockState iBlockStateIn) {
+		int blockMeta = iBlockStateIn.getBlock().getMetaFromState(iBlockStateIn);
+		if(iBlockStateIn.getBlock() == Blocks.GRASS_PATH) return true;
+		if(iBlockStateIn.getBlock() == BOP_GRASS_PATH && blockMeta <4) return true;
+		return false;
 	}
 
 	protected boolean isBlockDirt(IBlockState iBlockStateIn) {
@@ -71,6 +87,14 @@ public class Dirt2Path {
 		if(iBlockStateIn.getBlock() == Blocks.DIRT) return true;
 		if(iBlockStateIn.getBlock() == BOP_DIRT && blockMeta < 4) return true;
 		return false;
+	}
+
+	protected IBlockState getDirtBlockState(IBlockState iBlockState) {
+		int blockMeta = iBlockState.getBlock().getMetaFromState(iBlockState);
+		if(iBlockState.getBlock() == BOP_GRASS_PATH && blockMeta <4) {
+			return BOP_DIRT.getStateFromMeta(blockMeta);
+		}
+		return Blocks.DIRT.getDefaultState();
 	}
 
 	protected IBlockState getPathBlockState(IBlockState iBlockStateIn) {
